ccbe4899176b4c5083b0914c7da2c2b0ed84bae0
ewhol01-gmailcom <38090364+ewhol01-gmailcom@users.noreply.github.com>
2021-08-21 10:58:39 +0700

    Edit findswing module
    Now returns both hi & lo swing index & price simultaneously

--- findswing.py
+++ findswing.py
@@ -1,23 +1,25 @@
 import whpylib01 as wpl
 import pprint
 
-def find_swing(symbol="",interval="",type="H/L",start=-2,lookback=20):
-# def find_swing(symbol="",interval="",type="H/L",start=-1,lookback=20):
+def find_swing(symbol="",interval="",start=-2,lookback=20):
     candles = wpl.get_candlesticks(symbol=symbol,interval=interval,limit=lookback)
-    type = type.upper()
+    hi = 0 #hi index
+    hp = 0 #hi price
+    li = 0
+    lp = 0
     for i in range(start,-lookback+1,-1):
-        if type=="H":
-            print(i,candles[i][2])
-            if (candles[i][2]<candles[i-1][2]) and (candles[i-1][2]>candles[i-2][2]):
-                s = candles[i-1][2]
-                break
-        elif type=="L":
-            print(i,candles[i][3],candles[i-1][3],candles[i-2][3])
-            if (candles[i][3]>candles[i-1][3]) and (candles[i-1][3]<candles[i-2][3]):
-                s = candles[i-1][3]
-                break
-    # print(s)
-    return({"index":int(i-1),"price":float(s)})
+        # print(i,'h =',candles[i][2],candles[i-1][2],candles[i-2][2])
+        if (hi==0) and (candles[i][2]<candles[i-1][2]) and (candles[i-1][2]>candles[i-2][2]):
+            s = candles[i-1][2]
+            hp = float(s)
+            hi = i-1
+        # print(i,'l =',candles[i][3],candles[i-1][3],candles[i-2][3])
+        if (li==0) and (candles[i][3]>candles[i-1][3]) and (candles[i-1][3]<candles[i-2][3]):
+            s = candles[i-1][3]
+            lp = float(s)
+            li = i-1
+        if hi!=0 and li!=0:
+            break
+    return({"hiIndex":hi, "hiPrice":hp, "loIndex":li, "loPrice":lp})
 
-print(find_swing(symbol="DOGEUSDT",interval="15m",type="h",start=-2))
-print(find_swing(symbol="DOGEUSDT",interval="15m",type="l",start=-4))
+print(find_swing(symbol="DOGEUSDT",interval="15m",start=-2))
